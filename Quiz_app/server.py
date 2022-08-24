0c6ce12680090aca8b595d0e362f38f974097b01
R-Charles <Rapcharlo@aol.com>
2022-08-24 01:15:20 -0700

    updates to quiz project

---
+++ Quiz_app/server.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template  # Import Flask to allow us to create our app
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def index():
+    return render_template("index.html")
+
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
+
