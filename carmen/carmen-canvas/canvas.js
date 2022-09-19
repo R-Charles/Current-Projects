9c7db3bd4471c14cc8b8e0bb07f043c34f22cdf3
R-Charles <Rapcharlo@aol.com>
2022-09-19 13:55:03 -0700

    update

--- carmen/carmen-canvas/canvas.js
+++ carmen/carmen-canvas/canvas.js
@@ -48,44 +48,66 @@ function randomColor(){
 //     c.stroke();
 // }
 
-function Circle() {
+
+function Circle(x, y, dx, dy, radius) {
     this.x = x;
     this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
 
     this.draw = function(){
         c.beginPath();
-        c.arc(x, y, radius, 0, Math.PI * 2, false);
-        c.strokeStyle = 'purple';
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.strokeStyle = randomColor('purple');
         c.stroke();
+    }
+    this.update = function(){
+        if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+            this.dx = -this.dx;
+        }
+        if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+            this.dy = -this.dy;
+        }
+    
+        this.x += this.dx;
+        this.y += this.dy;
+
+        this.draw();
     }
 }
 
-var circle = new Circle(200, 200);
-// circle.draw();
+
+
+
+var circleArray = [];
+
+for (var i = 0; i < 100; i++) {
+    var radius = 30;
+    var x = Math.random() * innerWidth;
+    var y = Math.random() * innerHeight;
+    var dx = (Math.random() -0.5) *8;
+    var dy = (Math.random() -0.5) *8;
+    circleArray.push(new Circle(x, y, dx, dy, radius));  
+}
+
+console.log(circleArray);
+
     
-var x = Math.random() * innerWidth;
-var y = Math.random() * innerHeight;
-var dx = (Math.random() -0.5) *10;
-var dy = (Math.random() -0.5) *10;
-var radius = 30;
+
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, innerWidth, innerHeight);
 
-    c.beginPath();
-    c.arc(x, y, radius, 0, Math.PI * 2, false);
-    c.strokeStyle = randomColor('purple');
-    c.stroke();
-
-    if (x + radius > innerWidth || x - radius < 0) {
-        dx = -dx;
+    for (var i = 0; i < circleArray.length; i++) {
+        circleArray[i].update();
     }
-    if (y + radius > innerHeight || y - radius < 0) {
-        dy = -dy;
-    }
+    // c.beginPath();
+    // c.arc(x, y, radius, 0, Math.PI * 2, false);
+    // c.strokeStyle = randomColor('purple');
+    // c.stroke();
 
-    x += dx;
-    y += dy;
+
 }
 
  animate();
